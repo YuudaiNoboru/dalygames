59eaa641f574bce7ca81f886e9038a9457c16482
Lucas Vasconcelos <vasconcelos.contabies@gmail.com>
2025-04-02 20:50:54 -0300

    Mais alguns pequenos avanços no projeto.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { Container } from "@/components/container";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex">
-      <h1>Teste</h1>
-
+      <Container>
+        <h1 className="text-center font-bold text-xl mt-8 mb-5">Separamos um jogo exclusivo para você</h1>
+      </Container>
     </main>
   );
 }
